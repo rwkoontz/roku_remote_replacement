a04028fa50bd524e404622a6722e4651475be208
pyplot <rwkoontz@gmail.com>
2020-04-29 10:44:07 -0600

    Added ssdp.py to the project to add scan ability. Replaced the hardcoded ip address with a string of either the previously gathered address or the newly scanned address

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import os
+import ssdp
 
 class Handler:
     def on_rokuWindow_destroy(self, *args):
@@ -11,99 +12,115 @@ class Handler:
         Gtk.main_quit()
 
     def on_powerButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Power"
+        command = "curl -d '' " + mylocation + "keypress/Power"
         os.system(command)
         print("POWER\n")
 
     def on_backButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Back"
+        command = "curl -d '' " + mylocation + "keypress/Back"
         os.system(command)
         print("back\n")
 
     def on_homeButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Home"
+        command = "curl -d '' " + mylocation + "keypress/Home"
         os.system(command)
         print("home\n")
 
     def on_vupButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/VolumeUp"
+        command = "curl -d '' " + mylocation + "keypress/VolumeUp"
         os.system(command)
         print("VolUP\n")
 
     def on_vdwnButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/VolumeDown"
+        command = "curl -d '' " + mylocation + "keypress/VolumeDown"
         os.system(command)
         print("VolDWNP\n")
 
     def on_muteButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/VolumeMute"
+        command = "curl -d '' " + mylocation + "keypress/VolumeMute"
         os.system(command)
         print("mute\n")
 
     def on_downButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Down"
+        command = "curl -d '' " + mylocation + "keypress/Down"
         os.system(command)
         print("down\n")
 
     def on_upButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Up"
+        command = "curl -d '' " + mylocation + "keypress/Up"
         os.system(command)
         print("up\n")
 
     def on_leftButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Left"
+        command = "curl -d '' " + mylocation + "keypress/Left"
         os.system(command)
         print("left\n")
 
     def on_rightButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Right"
+        command = "curl -d '' " + mylocation + "keypress/Right"
         os.system(command)
         print("right\n")
 
     def on_selectButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Select"
+        command = "curl -d '' " + mylocation + "keypress/Select"
         os.system(command)
         print("select\n")
 
     def on_playButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Play"
+        command = "curl -d '' " + mylocation + "keypress/Play"
         os.system(command)
         print("play\n")
 
     def on_revButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Rev"
+        command = "curl -d '' " + mylocation + "keypress/Rev"
         os.system(command)
         print("reverse\n")
 
     def on_fwdButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/Fwd"
+        command = "curl -d '' " + mylocation + "keypress/Fwd"
         os.system(command)
         print("forward\n")
 
     def on_hdmi1Button_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/InputHDMI1"
+        command = "curl -d '' " + mylocation + "keypress/InputHDMI1"
         os.system(command)
         print("hdmi 1\n")
 
     def on_hdmi2Button_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/InputHDMI2"
+        command = "curl -d '' " + mylocation + "keypress/InputHDMI2"
         os.system(command)
         print("hdmi 2\n")
 
     def on_hdmi3Button_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/InputHDMI3"
+        command = "curl -d '' " + mylocation + "keypress/InputHDMI3"
         os.system(command)
         print("hdmi 3\n")
 
     def on_antButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/keypress/InputTuner"
+        command = "curl -d '' " + mylocation + "keypress/InputTuner"
         os.system(command)
         print("antenna\n")
 
     def on_netflixButton_clicked(self, button):
-        command = "curl -d '' http://192.168.1.142:8060/launch/12"
+        command = "curl -d '' " + mylocation + "launch/12"
         os.system(command)
         print("netflix\n")
+
+    def on_scanButton_clicked(self, button, *args):
+        mylocation = ssdp.discover("roku:ecp")
+        Foundlabel.set_text(mylocation)
+        with open("IPADDRESS.txt", "w") as text_file:
+            print(mylocation, file=text_file)
+        print(mylocation + "\n")
+
+mylocation = "none"
+try:
+    with open('IPADDRESS.txt', 'r') as myfile:
+        mylist=myfile.readline().split("\n")
+        mylocation=mylist[0]
+        print(type(mylocation))
+except:
+    print("no device address file found\n")
 
 builder = Gtk.Builder()
 builder.add_from_file("/home/pyplot/roku_remote_replacement/roku_remote_replacement/main.glade")
@@ -111,5 +128,10 @@ builder.connect_signals(Handler())
 
 window = builder.get_object("rokuWindow")
 window.show_all()
+
+Foundlabel = builder.get_object("foundLabel")
+Foundlabel.set_text(mylocation)
+
+
 
 Gtk.main()
